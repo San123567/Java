bae31c9d71720a8d0acea5f37249974ee967e776
San123567 <sanikasahu8@gmail.com>
2026-02-08 22:00:30 +0530

    Character to Uppercase Using Character.toUpperCase()

---
+++ Upperchar.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class WsCube {
+    public static void main(String[] args) {
+        
+        // Lowercase character 
+        char lower = 'w';  
+        
+        // Method to convert
+        char upper = Character.toUpperCase(lower); 
+        System.out.println("Uppercase: " + upper);
+    }
+}
