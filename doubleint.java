34ce5daf3c2e53b699f4e28fd9162de9381bb7b0
San123567 <sanikasahu8@gmail.com>
2026-02-08 21:27:57 +0530

    Double to Int Using Typecasting

---
+++ doubleint.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class WsCube {
+    public static void main(String[] args) {
+        
+        // Store floating-point value
+        double value = 37.89;           
+        
+        // Cast double to int
+        int result = (int) value;       
+        System.out.println("converted: " + result);
+    }
+}
