0817446b4185c44f5c6372a0450e0d8174731704
San123567 <sanikasahu8@gmail.com>
2026-02-08 21:51:16 +0530

    Multiplication Table Program Using While Loop

---
+++ loop.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class new {
+    public static void main(String[] args) {
+        int num = 3;
+        int i = 1;
+
+        // Using a while loop from 1 to 10
+        while(i<=10){
+            System.out.println(num + " x " + 
+                      i + " = " + (num * i));
+            i++;
+        }
+    }
+}
