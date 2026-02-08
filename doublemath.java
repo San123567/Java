1f239dc36a1bac0eec2cdc76db26a7308fbae606
San123567 <sanikasahu8@gmail.com>
2026-02-08 21:28:55 +0530

    Double to Int  Using Math.round()

---
+++ doublemath.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class WsCube {
+    public static void main(String[] args) {
+        double value = 37.89;
+        
+        // Round down to nearest int
+        int result = (int) Math.round(value); 
+        System.out.println("converted: " + result);
+    }
+}
