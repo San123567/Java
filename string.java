7fbc2562ba675c470ee2a416cec9dbd008fdd314
San123567 <sanikasahu8@gmail.com>
2026-02-08 21:31:13 +0530

    String to Double in Java Using Double.parseDouble()

---
+++ string.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class WsCube {
+    public static void main(String[] args) {
+        String price = "99.99";  
+
+        // Direct conversion
+        double doubleValue = Double.parseDouble(price);  
+        System.out.println("Converted: " + doubleValue);
+    }
+}
